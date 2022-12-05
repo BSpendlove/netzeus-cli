d4bf1b358b7bd69687dcc71b6cd89909046328a6
Brandon Spendlove <brandon.spendlove@netzeus.io>
2022-12-05 22:48:52 +0000

    Testing long description fix

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fs:
-    long_description = fs.read()
-
 setup(
     name="netzeus_cli",
     version="0.0.1",
     description="CLI Tool built by Network Engineers for Network Engineers",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description="CLI Tool built by Network Engineers for Network Engineers",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
